886c38e14086d5901a7fa4d369e3891f62e1e130
Andres Alvarez <andravid019@gmail.com>
2023-09-16 15:01:27 -0500

    todo

---
+++ src/POLINIMIOS/Nodo.java
@@ -0,0 +1,40 @@
+package POLINIMIOS;
+
+public class Nodo {
+       //atributos
+       private int coe, exp;
+       private Nodo Liga =null;
+
+        public Nodo(){
+            coe=0;
+            exp=0;
+            Liga=null;
+        }
+         public Nodo(int coe, int exp){
+            this.coe=coe;
+            this.exp=exp;  
+            Liga=null;      
+        }
+        public int getCoe() {
+            return coe;
+        }
+        public void setCoe(int coe) {
+            this.coe = coe;
+        }
+        public int getExp() {
+            return exp;
+        }
+        public void setExp(int exp) {
+            this.exp = exp;
+        }
+        public Nodo getLiga() {
+            return Liga;
+        }
+        public void setLiga(Nodo liga) {
+            Liga = liga;
+        }
+          
+       
+   
+    
+}
